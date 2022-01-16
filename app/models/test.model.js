1b21c35946370f11dadf7bc6b506028f0dd238df
nikhilshahu <97492634+nikhilshahu@users.noreply.github.com>
2022-01-16 12:32:55 +0530

    front-end that needs id field instead of _id

--- app/models/test.model.js
+++ app/models/test.model.js
@@ -6,4 +6,10 @@ const AppSchema = mongoose.Schema({
   summary: String,
 });
 
+AppSchema.method("toJSON", function() {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
 module.exports = mongoose.model("App", AppSchema);
